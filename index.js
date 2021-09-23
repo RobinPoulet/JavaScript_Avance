d2a228307878a2f6ac42cd187c3f0a612a447946
robin.poulet <robin.poulet@le-campus-numerique.fr>
2021-09-23 12:05:12 +0200

    third commit

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname + '/html/hello.html');
+})
+
+app.get('/page2', (req, res) => {
+    res.sendFile(__dirname + '/html/page2.html');
+})
+
+app.get('/page/:id', (req, res) => {
+    console.log(req.params.id);
+    res.sendFile(__dirname + '/html/page2.html')
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.use(express.static('public'));
+
