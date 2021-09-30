cd59a450546bc31d1ea1ecaccffa37ee158c7102
robin.poulet <robin.poulet@le-campus-numerique.fr>
2021-09-30 14:29:58 +0200

    ajut en tête

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "localhost:8080"),
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
+        next()
+});
+
 // Routes pour la partie Movie
 // app.get('/api/movies', (req, res) => {
 //     res.status(200).json(movies);
