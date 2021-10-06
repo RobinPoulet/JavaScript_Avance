99f7981a82a235885249a60dd3638eed4538d676
robin.poulet <robin.poulet@le-campus-numerique.fr>
2021-10-06 15:49:55 +0200

    maj

--- index.js
+++ index.js
@@ -190,7 +190,10 @@ app.delete('/api/moviesLists/:idList/:idFilm', async (req, res) => {
     try {
         console.log(req.params);
         let movieList = await dbMoviesList.get(req.params.idList);
-        movieList.film = movieList.film.filter(item => item !== req.params.idFilm);
+        let index = movieList.film.indexOf(req.params.id);
+        if (index > -1) {
+            movieList.film.splice(index, 1);
+        }
         await dbMoviesList.put(req.params.idList, movieList);
         res.status(204).json("l'élement a bien été effacé");
     } catch (error) {
